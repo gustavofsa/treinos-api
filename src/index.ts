8dd590b4ceddc95bdf8729ed5b23da473c216203
Gustavo Farias <gugafarias2009@gmail.com>
2026-03-08 20:53:52 -0300

    feat: add fastify api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello world!");
+import Fastify from "fastify";
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async function handler() {
+  return { hello: "world" };
+});
+
+try {
+  await fastify.listen({ port: Number(process.env.PORT) || 8080 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
